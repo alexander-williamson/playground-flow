55f0f14fb109f89cd8ceac68beeb999df394f86f
Alexander Williamson <alex@alexw.co.uk>
2013-10-13 20:56:25 +0100

    Prettify HTML

--- Flow.Library/UI/FormTemplateBase.cs
+++ Flow.Library/UI/FormTemplateBase.cs
@@ -35,7 +35,7 @@ namespace Flow.Library.UI
             Head = GetHead();
             Body = GetBody();
             Html = GetHtml(Body, Head);
-            Html = FormatHtml(Html);
+            Html = Html.Prettify();
         }
 
         private string GetHead()
@@ -77,38 +77,6 @@ namespace Flow.Library.UI
             sb.AppendLine("	</body>");
             sb.AppendLine("</html>");
             return sb.ToString();
-        }
-
-        public string FormatHtml(string content)
-        {
-            // adapted from http://stackoverflow.com/questions/1123718/format-xml-string-to-print-friendly-xml-string
-            try
-            {
-                var mStream = new MemoryStream();
-                var writer = new XmlTextWriter(mStream, Encoding.Unicode) {Formatting = Formatting.Indented};
-                var document   = new XmlDocument();
-
-                // Load the XmlDocument with the XML.
-                document.LoadXml(content);
-                writer.Formatting = Formatting.Indented;
-
-                // Write the XML into a formatting XmlTextWriter
-                document.WriteContentTo(writer);
-                writer.Flush();
-                mStream.Flush();
-
-                // Rewind the MemoryStream in order to read its contents.
-                mStream.Position = 0;
-
-                var sReader = new StreamReader(mStream);
-                return sReader.ReadToEnd();
-            } 
-            catch(Exception ex)
-            {
-                Console.WriteLine("An error occurred in FormatHtml. Message: " + ex.Message);
-                Console.WriteLine(ex);
-                return content;
-            }
         }
 
     }
---
+++ Flow.Library/UI/HtmlExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Flow.Library.UI
+{
+    public static class HtmlExtensions
+    {
+        public static string Prettify(this string content)
+        {
+            // adapted from http://stackoverflow.com/questions/1123718/format-xml-string-to-print-friendly-xml-string
+            try
+            {
+                var mStream = new MemoryStream();
+                var writer = new XmlTextWriter(mStream, Encoding.Unicode) {Formatting = Formatting.Indented};
+                var document   = new XmlDocument();
+
+                // Load the XmlDocument with the XML.
+                document.LoadXml(content);
+                writer.Formatting = Formatting.Indented;
+
+                // Write the XML into a formatting XmlTextWriter
+                document.WriteContentTo(writer);
+                writer.Flush();
+                mStream.Flush();
+
+                // Rewind the MemoryStream in order to read its contents.
+                mStream.Position = 0;
+
+                var sReader = new StreamReader(mStream);
+                return sReader.ReadToEnd();
+            } 
+            catch(Exception ex)
+            {
+                Console.WriteLine("An error occurred in FormatHtml. Message: " + ex.Message);
+                Console.WriteLine(ex);
+                return content;
+            }
+        }
+    }
+}
