5f6dcc3a7816e6862d5bad51ba200b631f0f5ba1
Alexander Williamson <alex@alexw.co.uk>
2013-10-13 20:54:47 +0100

    Added centralised flow repository for ease

---
+++ Flow.Library/Data/Repositories/FlowRepository.cs
@@ -0,0 +1,27 @@
+﻿using Flow.Library.Core;
+using Flow.Library.Steps;
+using Flow.Library.Validation;
+using Flow.Library.Validation.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flow.Library.Data.Repositories
+{
+    public class FlowRepository
+    {
+        public static FlowInstance GetFlow(int FlowId, int InstanceId)
+        {
+            var template = new FlowTemplate();
+            template.Variables.Add("yourName", string.Empty);
+            template.Steps.Add(new DataCollectionStep
+            {
+                ExitRules = new List<ValidationRule> { new ValidationRule { Key = "yourName", Validator = new StringRequired() } }
+            });
+            template.Steps.Add(new StoreDataStep());
+            //template.Steps.Add(new ShowInformationPage { Page = "Example" });
+            return new FlowInstance(template);;
+        }
+    }
+}
