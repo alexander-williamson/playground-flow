115860e31179c04781883209205399b1126ab183
Alexander Williamson <alex@alexw.co.uk>
2014-04-20 09:48:49 +0100

    Refactoring FlowTemplateStepController, extracting Map to static function

--- Web/Web/Controllers/Api/FlowTemplateStepsController.cs
+++ Web/Web/Controllers/Api/FlowTemplateStepsController.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web;
 using System.Web.Http;
 using AutoMapper;
 using Flow.Library.Data;
@@ -11,7 +9,6 @@ using Flow.Library.Steps;
 using Flow.Library.Validation;
 using Flow.Web.Dto;
 using FlowTemplate = Flow.Library.Core.FlowTemplate;
-using FlowTemplateStep = Flow.Library.Core.FlowTemplateStep;
 
 namespace Flow.Web.Controllers.Api
 {
@@ -19,47 +16,47 @@ namespace Flow.Web.Controllers.Api
     {
 
         private readonly IUnitOfWork _unitOfWork;
-        private readonly FlowTemplateService _flowTemplateService;
+        private readonly FlowTemplateService _templates;
 
         public FlowTemplateStepsController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
-            _flowTemplateService = new FlowTemplateService();
+            _templates = new FlowTemplateService();
         }
 
         public HttpResponseMessage Get(int parent)
         {
-            var steps = _flowTemplateService.GetFlowTemplateSteps(_unitOfWork, parent);
+            var steps = _templates.GetFlowTemplateSteps(_unitOfWork, parent);
             var stepsArray = steps as IStep[] ?? steps.ToArray();
 
-            if (steps == null || !stepsArray.Any())
+            if (!stepsArray.Any())
                 return Request.CreateResponse(HttpStatusCode.NotFound);
 
-            var mappedSteps = stepsArray.Select(Mapper.Map<FlowTemplateStepDto>).ToList();
+            var mappedSteps = stepsArray.Select(Map<FlowTemplateStepDto>).ToList();
             return Request.CreateResponse(HttpStatusCode.OK, mappedSteps);
         }
 
         public HttpResponseMessage Get(int parent, int id)
         {
-            var steps = _flowTemplateService.GetFlowTemplateSteps(_unitOfWork, parent).ToList();
+            var steps = _templates.GetFlowTemplateSteps(_unitOfWork, parent).ToList();
             var matching = steps.Where(o => o.Id == id).ToList();
             
             if (!matching.Any())
                 return Request.CreateResponse(HttpStatusCode.NotFound);
             
-            var mappedDtoFlowTemplate = AutoMapper.Mapper.Map<FlowTemplateStepDto>(matching.First());
+            var mappedDtoFlowTemplate = Map<FlowTemplateStepDto>(matching.First());
             return Request.CreateResponse(HttpStatusCode.OK, mappedDtoFlowTemplate);
         }
 
         public HttpResponseMessage Delete(int parent, int id)
         {
-            var parentSteps = _flowTemplateService.GetFlowTemplateSteps(_unitOfWork, parent);
+            var parentSteps = _templates.GetFlowTemplateSteps(_unitOfWork, parent);
             var matching = parentSteps.Where(o => o.Id == id).ToList();
 
             if (!matching.Any())
                 return Request.CreateResponse(HttpStatusCode.NotFound);
 
-            _flowTemplateService.Delete(_unitOfWork, new FlowTemplate { Id = id });
+            _templates.Delete(_unitOfWork, new FlowTemplate { Id = id });
             return Request.CreateResponse(HttpStatusCode.OK);
         }
 
@@ -68,8 +65,8 @@ namespace Flow.Web.Controllers.Api
 
             try
             {
-                var mapped = Mapper.Map<IStep>(instance);
-                var id = _flowTemplateService.Add(_unitOfWork, mapped, parent);
+                var mapped = Map<IStep>(instance);
+                var id = _templates.Add(_unitOfWork, mapped, parent);
                 _unitOfWork.Commit();
                 return Request.CreateResponse(HttpStatusCode.OK, new { Id = id });
             }
@@ -77,6 +74,11 @@ namespace Flow.Web.Controllers.Api
             {
                 return Request.CreateResponse(HttpStatusCode.BadRequest, new { ex.BrokenRules });
             }
+        }
+
+        public static T Map<T>(object source)
+        {
+            return Mapper.Map<T>(source);
         }
     }
 }
